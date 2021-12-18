e898f12786de02bb323c8f572f994c4df34c2fe7
RobbeBeversAP <72867753+RobbeBeversAP@users.noreply.github.com>
2021-12-19 00:51:41 +0100

    Final changes

--- persist/repo.ts
+++ persist/repo.ts
@@ -1,23 +1,17 @@
 import {getPool} from "./db"
 
-
-
 export function addGifTask(task : GifTask)
 {
     getPool().query(`INSERT INTO GifTask (id, userId, outputObjectId, featured)
     VALUES (NEXTVAL(gifseq), "${task.userId}" ,"${task.outputObjectId}", ${task.featured});`)
-
    
 }
 
 export function getAllFeaturedGifsFromUser(userId: string) : Promise<any>
 {
     let val : Promise<any> = getPool().query(`SELECT outputObjectId FROM GifTask WHERE featured=1 AND userId="${userId}"`)
-   
     return val
 }
-
-
 
 export interface GifTask{
     id?: number
